b41fd174c43bcfbbec76a8c1f7c03b7357a28ee8
Evan Lane <evanklane@yahoo.com>
2018-11-04 22:04:13 -0500

    added bid logic

--- public/script.js
+++ public/script.js
@@ -3,21 +3,29 @@
 const socket = io.connect('http://localhost:3000');
 
 //query dom
-const message = document.getElementById('message');
+const bid = document.getElementById('bid');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+const bidup = document.getElementById('bidup');
 
 // emit event
 
 btn.addEventListener('click', function(){
   socket.emit('chat',{
-    message: message.value,
-    handle: handle.value
+    bid: bid.value,
+    handle: socket.id
+  })
+})
+
+bidup.addEventListener('click',function(){
+  socket.emit('chat',{
+    bid: 'bidup',
+    handle: socket.id
   })
 })
 
 //listen for events
 socket.on('chat',function(data){
-  output.innerHTML += `<p>${data.handle}: ${data.message}</p>`
+  output.innerHTML = `<p>${data[data.length-1].handle}: ${data[data.length-1].bid}</p>`
 })
--- server.js
+++ server.js
@@ -13,7 +13,23 @@ const io = socket(server);
 io.on('connection',function(socket){
   console.log(`socket works dude at ${socket.id}`)
 
+const bidLedger = [{bid: 1, handle:'default'}];
+
   socket.on('chat', function(data){
-    io.emit('chat',data);
+    if(data.bid == 'bidup'){
+      data.bid = bidLedger[bidLedger.length-1].bid + 1
+      bidLedger.push(data);
+    } else {
+    //   console.log(`new bid: ${data.bid}`)
+    // console.log(`last valid bid: ${bidLedger[bidLedger.length-1].bid}`)
+    // console.log(data.bid > bidLedger[bidLedger.length-1].bid)
+    //only add bid to bid ledger if it is valid
+       data.bid = parseInt(data.bid)
+      if(data.bid > bidLedger[bidLedger.length-1].bid){
+        bidLedger.push(data);
+      }
+    }
+    console.log(bidLedger)
+    io.emit('chat',bidLedger);
   })
 })
