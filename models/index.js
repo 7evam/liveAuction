84d91e3fd6316b5d2eabf6d8af91658a8083c697
Evan Lane <evanklane@yahoo.com>
2019-01-07 13:49:13 -0500

    new db url

--- models/index.js
+++ models/index.js
@@ -2,38 +2,13 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-// const db = new Sequelize({
-//   database: 'live_auction',
-//   dialect:  'postgres',
-//   define:   {
-//     underscored:   true,
-//     returning:     true,
-//   },
-// });
-
-// const db = new Sequelize({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-//   dialect:  'postgres',
-//   define: {
-//     underscored:   true,
-//     returning:     true,
-//   },
-// });
-
 const db = new Sequelize(
-  process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  underscored: true,
-  returning: true,
-});
-
-// const db = new Sequelize(
-//   process.env.DATABASE_URL,
-// {
-//     underscored: true,
-//     returning: true,
-//   });
+  process.env.DATABASE_URL,
+{
+    underscored: true,
+    returning: true,
+  },
+);
 
 const Item = db.define('item', {
   name: {
