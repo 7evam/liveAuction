8ce566cafe810576d47520f5d0a0d5be2daebbae
Evan Lane <evanklane@yahoo.com>
2019-01-07 11:58:31 -0500

    try something else

--- models/index.js
+++ models/index.js
@@ -14,6 +14,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
+  dialect:  'postgres',
   define: {
     underscored:   true,
     returning:     true,
